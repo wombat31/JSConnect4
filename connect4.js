365d4cd2dd827d549d72bff9e208b4a117966a65
Justin Welham <jwelham@BCAA.AE>
2018-12-06 11:59:09 +0400

    game frame added

--- connect4.js
+++ connect4.js
@@ -4,7 +4,7 @@ var myGameGrid;
 function startGame() {
     myGameArea.start();
     myGamePiece = new component(480, 270, "black", 0, 0);
-    myGameGrid = new component(10,200,"green",300,120);
+    myGameGrid = new framePiece(100,100,100,100,50,0,0,"red");
 }
 
 var myGameArea = {
@@ -27,3 +27,21 @@ function component(width, height, color, x, y,type,radius) {
     ctx.fillRect(this.x, this.y, this.width, this.height);
 }
 
+function framePiece(width, height, x, y, radius, startAngle, endAngle,color){
+    this.width = width;
+    this.height = height;
+    this.x = x;
+    this.y = y;
+    ctx= myGameArea.context;
+    ctx.beginPath();
+    ctx.moveTo(50,100);
+    ctx.lineTo(0,100);
+    ctx.lineTo(0,0);
+    ctx.lineTo(100,0);
+    ctx.arc(100,100,50,3*Math.PI/2, Math.PI,true);
+    ctx.closePath()
+    ctx.fillStyle = color;
+    ctx.fill();
+    ctx.strokeStyle = color;
+    ctx.stroke();
+}
