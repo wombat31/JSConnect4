6171cb95c1541a9b1d332e29ed8306bd167829d1
Justin Welham <jwelham@BCAA.AE>
2018-12-06 11:32:24 +0400

    canvas working

---
+++ connect4.js
@@ -0,0 +1,29 @@
+var myGamePiece;
+var myGameGrid;
+
+function startGame() {
+    myGameArea.start();
+    myGamePiece = new component(480, 270, "black", 0, 0);
+    myGameGrid = new component(10,200,"green",300,120);
+}
+
+var myGameArea = {
+    canvas : document.createElement("canvas"),
+    start : function() {
+        this.canvas.width = 480;
+        this.canvas.height = 270;
+        this.context = this.canvas.getContext("2d");
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+    }
+}
+
+function component(width, height, color, x, y,type,radius) {
+    this.width = width;
+    this.height = height;
+    this.x = x;
+    this.y = y; 
+    ctx = myGameArea.context;
+    ctx.fillStyle = color;
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+}
+
